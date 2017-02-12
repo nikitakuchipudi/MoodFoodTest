701c0b300b229f7422cb3af4ebf646eb1d3d899a
nxk6769 <nxk6769@cs.rit.edu>
2017-02-12 11:09:57 -0500

    Initial submission

--- moodfood/moods/admin.py
+++ moodfood/moods/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Restaurant, Mood, Type
+
+admin.site.register(Restaurant)
+
+admin.site.register(Mood)
+
+admin.site.register(Type)
--- moodfood/moods/urls.py
+++ moodfood/moods/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<mood_type>[0-9]+)/$', views.moodDetail, name='MoodDetail'),
-    url(r'^(?P<mood_type>[0-9]+)/restaurantRec/$', views.restaurantRec, name='restaurantRec'),
-    url(r'^(?P<mood_type>[0-9]+)/restaurantApprove/$', views.restaurantApprove, name='restaurantApprove'),
+    url(r'^(?P<mood_type>[a-z]+)/$', views.moodDetail, name='MoodDetail'),
+    url(r'^(?P<mood_type>[a-z]+)/restaurantRec/$', views.restaurantRec, name='restaurantRec'),
+    url(r'^(?P<mood_type>[a-z]+)/restaurantApprove/$', views.restaurantApprove, name='restaurantApprove'),
 ]
